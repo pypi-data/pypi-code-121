2e4b5b2ec36d938cda7e04e321311090a67fb36e
Tom Forbes <tom@tomforb.es>
2022-05-24 07:30:53 +0000

    {"name":"cfan-2017-part3","version":"2022.5.24.0","file":"cfan-2017-part3-2022.5.24.0.tar.gz","path":"cfan-2017-part3/cfan-2017-part3-2022.5.24.0.tar.gz"}

---
+++ cfan-2017-part3/cfan-2017-part3-2022.5.24.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import Cfan2017Part3
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('Cfan2017Part3'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="cfan-2017-part3",
+    version=Cfan2017Part3.__version__,
+    url="https://github.com/apachecn/cfan-2017-part3",
+    author=Cfan2017Part3.__author__,
+    author_email=Cfan2017Part3.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="电脑爱好者2017合订本（三）",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "cfan-2017-part3=Cfan2017Part3.__main__:main",
+            "Cfan2017Part3=Cfan2017Part3.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
