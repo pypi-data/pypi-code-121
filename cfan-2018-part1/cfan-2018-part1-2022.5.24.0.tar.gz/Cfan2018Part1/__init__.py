bc1a9cc18c7d4458a6793f3f0d4de0a93f14b324
Tom Forbes <tom@tomforb.es>
2022-05-24 07:37:11 +0000

    {"name":"cfan-2018-part1","version":"2022.5.24.0","file":"cfan-2018-part1-2022.5.24.0.tar.gz","path":"cfan-2018-part1/cfan-2018-part1-2022.5.24.0.tar.gz"}

---
+++ cfan-2018-part1/cfan-2018-part1-2022.5.24.0.tar.gz/Cfan2018Part1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""cfan-2018-part1
+https://github.com/apachecn/cfan-2018-part1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.24.0"
