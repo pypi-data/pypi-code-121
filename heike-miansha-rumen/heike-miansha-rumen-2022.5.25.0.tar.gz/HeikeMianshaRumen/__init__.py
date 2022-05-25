f5f5b86fb348bfaed221711825894b629260e1c8
Tom Forbes <tom@tomforb.es>
2022-05-25 07:43:50 +0000

    {"name":"heike-miansha-rumen","version":"2022.5.25.0","file":"heike-miansha-rumen-2022.5.25.0.tar.gz","path":"heike-miansha-rumen/heike-miansha-rumen-2022.5.25.0.tar.gz"}

---
+++ heike-miansha-rumen/heike-miansha-rumen-2022.5.25.0.tar.gz/HeikeMianshaRumen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""heike-miansha-rumen
+https://github.com/apachecn/heike-miansha-rumen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.25.0"
