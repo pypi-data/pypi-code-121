33f25d8eaff20cac206e1e4f9b0286a16608a656
Tom Forbes <tom@tomforb.es>
2022-05-25 07:03:49 +0000

    {"name":"heifang-huanchongqu-yichu-zhuanji","version":"2022.5.25.0","file":"heifang-huanchongqu-yichu-zhuanji-2022.5.25.0.tar.gz","path":"heifang-huanchongqu-yichu-zhuanji/heifang-huanchongqu-yichu-zhuanji-2022.5.25.0.tar.gz"}

---
+++ heifang-huanchongqu-yichu-zhuanji/heifang-huanchongqu-yichu-zhuanji-2022.5.25.0.tar.gz/HeifangHuanchongquYichuZhuanji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""heifang-huanchongqu-yichu-zhuanji
+https://github.com/apachecn/heifang-huanchongqu-yichu-zhuanji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.25.0"
