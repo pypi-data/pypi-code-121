1d0cd618e331147894b3e7f74e9cb704dde1882e
Tom Forbes <tom@tomforb.es>
2022-05-25 08:27:26 +0000

    {"name":"jiami-jiemi-jishu","version":"2022.5.25.0","file":"jiami-jiemi-jishu-2022.5.25.0.tar.gz","path":"jiami-jiemi-jishu/jiami-jiemi-jishu-2022.5.25.0.tar.gz"}

---
+++ jiami-jiemi-jishu/jiami-jiemi-jishu-2022.5.25.0.tar.gz/JiamiJiemiJishu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiami-jiemi-jishu
+https://github.com/apachecn/jiami-jiemi-jishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.25.0"
