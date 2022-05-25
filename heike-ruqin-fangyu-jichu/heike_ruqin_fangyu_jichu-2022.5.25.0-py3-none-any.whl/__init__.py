3619a391d0ad49dea3142f6d13f325d0b2fa3e42
Tom Forbes <tom@tomforb.es>
2022-05-25 07:51:57 +0000

    {"name":"heike-ruqin-fangyu-jichu","version":"2022.5.25.0","file":"heike_ruqin_fangyu_jichu-2022.5.25.0-py3-none-any.whl","path":"heike-ruqin-fangyu-jichu/heike_ruqin_fangyu_jichu-2022.5.25.0-py3-none-any.whl"}

---
+++ heike-ruqin-fangyu-jichu/heike_ruqin_fangyu_jichu-2022.5.25.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""heike-ruqin-fangyu-jichu
+https://github.com/apachecn/heike-ruqin-fangyu-jichu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.25.0"
