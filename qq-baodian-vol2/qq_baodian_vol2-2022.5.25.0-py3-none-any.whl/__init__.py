d15b5aff430b1209507c71099b74d67d50ede5f6
Tom Forbes <tom@tomforb.es>
2022-05-25 08:40:29 +0000

    {"name":"qq-baodian-vol2","version":"2022.5.25.0","file":"qq_baodian_vol2-2022.5.25.0-py3-none-any.whl","path":"qq-baodian-vol2/qq_baodian_vol2-2022.5.25.0-py3-none-any.whl"}

---
+++ qq-baodian-vol2/qq_baodian_vol2-2022.5.25.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qq-baodian-vol2
+https://github.com/apachecn/qq-baodian-vol2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.25.0"
